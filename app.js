96d47b4a8327447f215984dcf23201718ae1d008
srisathyamamidala <77813332+srisathyamamidala@users.noreply.github.com>
2021-04-01 00:10:38 -0500

    all files added

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var treesRouter = require('./routes/trees');
+var starsRouter = require('./routes/stars');
+var slotRouter = require('./routes/slot');
+
 
 var app = express();
 
@@ -21,6 +25,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/trees', treesRouter);
+app.use('/stars', starsRouter);
+app.use('/slot', slotRouter);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
